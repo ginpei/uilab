3cc7449c8fedd55e6a2d2ba95c375c6daa113910
ginpei <ginpei@gmail.com>
2015-09-19 12:27:44 -0700

    get elements from the beginning

--- 3d-cube/js.js
+++ 3d-cube/js.js
@@ -1,21 +1,21 @@
-// FIXME
-document.querySelector('.ui-cube').onclick = function(event) {
-	var LEN_SURFACE = 6;
-
-	var elCube = event.currentTarget;
-	var surface = Number(elCube.getAttribute('data-uicube-surface')) || 1;
-
-	if (surface === LEN_SURFACE) {
-		elCube.setAttribute('data-uicube-surface', 1);
-	}
-	else {
-		elCube.setAttribute('data-uicube-surface', surface+1);
-	}
-};
-
 (function() {
+	var elCube = document.querySelector('.ui-cube');
 	var elVisibilities = document.querySelectorAll('.js-visibility');
 	var elSurfaces = document.querySelectorAll('.js-surface');
+
+	elCube.onclick = function(event) {
+		var LEN_SURFACE = 6;
+
+		var surface = Number(elCube.getAttribute('data-uicube-surface')) || 1;
+
+		if (surface === LEN_SURFACE) {
+			elCube.setAttribute('data-uicube-surface', 1);
+		}
+		else {
+			elCube.setAttribute('data-uicube-surface', surface+1);
+		}
+	};
+
 	function updateVisivilities() {
 		for (var i=0, l=elSurfaces.length; i<l; i++) {
 			elSurfaces[i].style.display = (elVisibilities[i].checked ? '' : 'none');
