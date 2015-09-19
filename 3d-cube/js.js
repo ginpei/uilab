3f7f45fda1cf3a56350c5680e1c94934e24dd81f
ginpei <ginpei@gmail.com>
2015-09-19 11:34:28 -0700

    toggle visibilities

--- 3d-cube/js.js
+++ 3d-cube/js.js
@@ -12,3 +12,19 @@ document.body.onclick = function(event) {
 		elCube.setAttribute('data-uicube-surface', surface+1);
 	}
 };
+
+(function() {
+	var elVisibilities = document.querySelectorAll('.js-visibility');
+	var fn = function(event) {
+		event.stopPropagation();
+
+		var nth = event.currentTarget.value;
+		var visibility = event.currentTarget.checked;
+		var elSurface = document.querySelectorAll('.js-surface')[nth-1];
+		elSurface.style.display = (visibility ? '' : 'none');
+	};
+	for (var i=0, l=elVisibilities.length; i<l; i++) {
+		var elVisibility = elVisibilities[i];
+		elVisibility.onclick = fn;
+	}
+})();
