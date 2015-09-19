abe445a98fe6284c244d651f553fbc4f60cce218
ginpei <ginpei@gmail.com>
2015-09-19 10:21:15 -0700

    define surfaceses' length

--- 3d-cube/js.js
+++ 3d-cube/js.js
@@ -1,9 +1,11 @@
 // FIXME
 document.body.onclick = function(event) {
+	var LEN_SURFACE = 4;
+
 	var elCube = document.querySelector('.ui-cube');
 	var surface = Number(elCube.getAttribute('data-uicube-surface')) || 1;
 
-	if (surface === 4) {
+	if (surface === LEN_SURFACE) {
 		elCube.setAttribute('data-uicube-surface', 1);
 	}
 	else {
