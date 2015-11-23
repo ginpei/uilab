f32f160f010915b72afbd81c89d8358573303641
ginpei <ginpei@gmail.com>
2015-11-22 21:35:38 -0800

    index

--- time-select/js.js
+++ time-select/js.js
@@ -1,5 +1,9 @@
 (function() {
 	function $(selector) {
+		if (!(this instanceof $)) {
+			return new $(selector);
+		}
+
 		var els;
 		var selectorText = null;
 		if (typeof selector === 'string') {
@@ -9,48 +13,63 @@
 		else {
 			els = [selector];
 		}
-		var obj = {
+		$.extend(this, {
 			selector: selectorText,
 			els: els,
-			length: els.length,
-			each: function(callback) {
-				for (var i=0, l=this.els.length; i<l; i++) {
-					callback(i, this.els[i]);
-				}
-				return this;
-			},
-			on: function(type, listener) {
-				this.each(function(index, el) {
-					el.addEventListener(type, listener);
-				});
-				return this;
-			},
-			css: function(props) {
-				return this.each(function(index, el) {
-					for (var prop in props) {
-						el.style[prop] = props[prop];
-					}
-				});
-			},
-			addClass: function(className) {
-				var classNames = className.split(' ');
-				return this.each(function(index, el) {
-					for (var i=0, l=classNames.length; i<l; i++) {
-						el.classList.add(classNames[i]);
-					}
-				});
-			},
-			removeClass: function(className) {
-				var classNames = className.split(' ');
-				return this.each(function(index, el) {
-					for (var i=0, l=classNames.length; i<l; i++) {
-						el.classList.remove(classNames[i]);
-					}
-				});
-			}
-		};
-		return obj;
+			length: els.length
+		});
+
+		this.each(function(index, el) {
+			this[index] = el;
+		});
 	}
+	$.extend = function(source) {
+		var objs = Array.prototype.slice.call(arguments, 1);
+		for (var i=0, l=objs.length; i<l; i++) {
+			var obj = objs[i];
+			for (var prop in obj) {
+				source[prop] = obj[prop];
+			}
+		}
+		return source;
+	};
+	$.extend($.prototype, {
+		each: function(callback) {
+			for (var i=0, l=this.els.length; i<l; i++) {
+				callback.call(this, i, this.els[i]);
+			}
+			return this;
+		},
+		on: function(type, listener) {
+			this.each(function(index, el) {
+				el.addEventListener(type, listener);
+			});
+			return this;
+		},
+		css: function(props) {
+			return this.each(function(index, el) {
+				for (var prop in props) {
+					el.style[prop] = props[prop];
+				}
+			});
+		},
+		addClass: function(className) {
+			var classNames = className.split(' ');
+			return this.each(function(index, el) {
+				for (var i=0, l=classNames.length; i<l; i++) {
+					el.classList.add(classNames[i]);
+				}
+			});
+		},
+		removeClass: function(className) {
+			var classNames = className.split(' ');
+			return this.each(function(index, el) {
+				for (var i=0, l=classNames.length; i<l; i++) {
+					el.classList.remove(classNames[i]);
+				}
+			});
+		}
+	});
 
 	var $document = $(document);
 	var $button = $('.js-selectTime');
@@ -70,7 +89,8 @@
 	});
 	$hourSelection.on('mousedown', function(event) {
 		event.stopPropagation();
-		console.log(event.target.firstChild.nodeValue);
+		var $button = $(event.target);
+		console.log($button[0].firstChild.nodeValue);
 	});
 	$button.on('click', function(event) {
 		$hourSelection.removeClass('active');
